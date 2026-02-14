44a232e03fbbd06e356340ff985a86017bb744c6
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2026-02-14 11:48:38 -0400

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Khaleb Lisboa on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreakerTests/CodeBreakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CodeBreakerTests.swift
+//  CodeBreakerTests
+//
+//  Created by Khaleb Lisboa on 14/02/26.
+//
+
+import Testing
+
+struct CodeBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
